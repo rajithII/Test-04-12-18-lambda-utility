17d21918109ef0360b3b1177b3f946a6fe8ecf82
Rajith <rajith.v@iinerds.com>
2018-12-07 13:46:26 +0530

    lambd list updated

--- build_lambda.sh
+++ build_lambda.sh
@@ -1,24 +1,24 @@
 #!/bin/bash      
 #title          :build_lambda.sh
-#description    :This script will build all lambdas/selected lambdas
+#description    :This script will build all lambdas/selected lambdas with the utility.
 #author         :rajith.v@iinerds.com
 #date           :06-12-2018
 #version        :1.0    
 #usage          :/bin/sh build_lambda.sh
 #bash_version   :4.2.46(1)-release
-#==============================================================================
+#======================================================================================
 
-#Update Lamda Alias with the new version created
+#Update Lambda Alias with the new version created
 update_lambda_alias () {
     echo "Creating new version.."
-    aws lambda publish-version --function-name $1 --region us-west-2  > version2.txt
+    aws lambda publish-version --function-name $1 --region us-east-1  > version2.txt
     if [ $? -eq 0 ]
     then
         version2=$(cat version2.txt | grep "Version" | awk '{ print $2 }' | tr -d '",')       
         echo "Version created successfully!. New version is: '$version2' "        
         echo "Updating lambda alias...."        
         sleep 30s
-        aws lambda update-alias --function-name $1 --name dev --function-version $version2 --region us-west-2 
+        aws lambda update-alias --function-name $1 --name dev --function-version $version2 --region us-east-1 
         if [ $? -eq 0 ]
             then                
                 echo "Updated 'Dev' alias with new version '$version2'"                
@@ -33,13 +33,13 @@ update_lambda_alias () {
 #Create Lambda Alias and update the alias with the new version
 create_lambda_alias () {   
     echo "Creating new version.."   
-    aws lambda publish-version --function-name $1 --region us-west-2  > version1.txt
+    aws lambda publish-version --function-name $1 --region us-east-1  > version1.txt
     if [ $? -eq 0 ]
     then
         version1=$(cat version1.txt | grep "Version" | awk '{ print $2 }' | tr -d '",')           
         echo "Version created successfully!. New version is: '$version1' "       
         echo "Creating new lambda alias...."         
-        aws lambda create-alias --function-name $1 --name dev --function-version $version1 --region us-west-2 
+        aws lambda create-alias --function-name $1 --name dev --function-version $version1 --region us-east-1 
     if [ $? -eq 0 ]
         then               
             echo "Created lamda alias 'Dev' with the new version '$version1'"              
@@ -54,7 +54,7 @@ create_lambda_alias () {
 
 #Check Lambda Alias
 check_lambda_alias () {
-    alias=$(aws lambda list-aliases --function-name $1 --region us-west-2  | grep -i name | grep dev | awk '{ print $2}' | tr -d ',"')
+    alias=$(aws lambda list-aliases --function-name $1 --region us-east-1  | grep -i name | grep dev | awk '{ print $2}' | tr -d ',"')
     if [ "$alias" == "dev" ]
     then 
         update_lambda_alias $1 
@@ -67,7 +67,7 @@ check_lambda_alias () {
 #Create new lambda function 
 create_lambda () {    
     echo "Creating new lambda with name '$1' is in progress"     
-    aws lambda create-function --function-name $1 --runtime java8 --handler LambdaFunctionHandler.java --role arn:aws:iam::902849442700:role/LambdaFullAccess --zip-file fileb://./target/demo-1.0.0.jar --region us-west-2 
+    aws lambda create-function --function-name $1 --runtime java8 --handler $2 --role arn:aws:iam::902849442700:role/LambdaFullAccess --zip-file fileb://./target/demo-1.0.0.jar --region us-east-1 
     if [ $? -eq 0 ]
     then            
         echo "Lambda function: '$1' has been created successfully"        
@@ -81,7 +81,7 @@ create_lambda () {
 #Update lambda function
 update_lambda () {     
     echo "Update porcess of lambda function: '$1' is in progress"      
-    aws lambda update-function-code --function-name $1 --zip-file fileb://./target/demo-1.0.0.jar --region us-west-2 
+    aws lambda update-function-code --function-name $1 --zip-file fileb://./target/demo-1.0.0.jar --region us-east-1 
     if [ $? -eq 0 ]
     then             
         echo "Update process of lambda function: '$1' has been completed"            
@@ -95,7 +95,7 @@ update_lambda () {
 check_lambda_exist () {
     check_flag=0     
     echo "Searching for '$1'"       
-    var=$(aws lambda list-functions --region us-west-2  | grep -i functionname | awk '{ print $2 }' | tr -d '",')
+    var=$(aws lambda list-functions --region us-east-1  | grep -i functionname | awk '{ print $2 }' | tr -d '",')
     for name in `echo $var`
     do 
 	        if [ "$name" == "$1" ]
@@ -107,7 +107,7 @@ check_lambda_exist () {
     done
     if [ $check_flag -eq 0 ]
     then
-        create_lambda $1
+        create_lambda $1 $3
     fi        
 }
 
@@ -120,7 +120,7 @@ build_lambda (){
     if [ $? -eq 0 ]
     then        
         echo "Build of '$2' with utility jar has been completed successfully"        
-        check_lambda_exist $2
+        check_lambda_exist $2 $3
     else        
         echo "Error during the lambda build with utility jar. Please fix the error and start over!"        
     fi
@@ -134,12 +134,13 @@ selective_build () {
         do       
             repo_name=$(echo "$line" | cut -d':' -f1)
             lambda_name=$(echo "$line" | cut -d':' -f2)
+            handler_name=$(echo "$line" | cut -d':' -f3)
             if [ "$lambda_name" == "$word" ];
             then
                 echo "=============================================="
                 echo "Building Lambda: $lambda_name"               
                 echo "=============================================="
-                build_lambda $repo_name $lambda_name
+                build_lambda $repo_name $lambda_name $handler_name
             fi 
         done
 done
@@ -153,10 +154,11 @@ echo "====================================="
 for line in `cat lambdalist.txt`
 do       
     repo_name=$(echo "$line" | cut -d':' -f1)
-    lambda_name=$(echo "$line" | cut -d':' -f2)    
+    lambda_name=$(echo "$line" | cut -d':' -f2)  
+    handler_name=$(echo "$line" | cut -d':' -f3)  
     echo "reponame: $repo_name"   
     echo "lambdaname: $lambda_name"   
-    build_lambda $repo_name $lambda_name 
+    build_lambda $repo_name $lambda_name $handler_name
 done
 }
 
@@ -171,10 +173,5 @@ fi
 }
 
 start
-
-
-
-
-
 
 
